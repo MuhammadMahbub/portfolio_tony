e01ded591bcb2eb4b9c3d51a3d5e5e4ec6bfbd74
Muhammad Mahbub <muhammadmahbub07@gmail.com>
2023-10-14 09:15:51 +0600

    setup

---
+++ app/Models/Project.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Skill;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function skill(){
+        return $this->belongsTo(Skill::class);
+    }
+}
---
+++ app/Models/Skill.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
+}
